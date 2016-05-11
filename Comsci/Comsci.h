1ffdd7201bc99253e60becd8d59836c041f9b2ed
1piedude11 <1piedude11@gmail.com>
2016-05-11 10:24:06 -0700

    Add gitignore

--- Comsci/Comsci.h
+++ Comsci/Comsci.h
@@ -1,3 +1,389 @@
 #pragma once
 
-#include "resource.h"
+#include "Resource.h"
+//Windows Header Files:
+#include <windows.h>
+
+//C RunTIme Header FIles:
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <wchar.h>
+#include <math.h>
+
+#pragma comment(lib, "d2d1")
+#include <d2d1helper.h>
+#include <dwrite.h>
+#include <wincodec.h>
+
+template <class Interface>
+inline void SafeRelease(
+	Interface **ppInterfaceToRelease
+	)
+{
+	if (*ppInterfaceToRelease != NULL)
+	{
+		(*ppInterfaceToRelease)->Release();
+		(*ppInterfaceToRelease) = NULL;
+	}
+}
+
+#ifndef Assert
+#if defined( DEBUG ) || defined( _DEBUG )
+#define Assert(b) do {if (!(b)) {OutputDebugStringA("Assert: " #b "\n");}} while(0)
+#else
+#define Assert(b)
+#endif //DEBUG || _DEBUG
+#endif
+
+
+#ifndef HINST_THISCOMPONENT
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+#define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
+#endif
+
+
+class Comsci
+{
+public:
+	Comsci();
+	~Comsci();
+
+	HRESULT Initialize();
+
+	void RunMessageLoop();
+
+private:
+	HRESULT CreateDeviceIndependentResources();
+
+	HRESULT CreateDeviceResources();
+
+	void DiscardDeviceResources();
+
+	HRESULT OnRender();
+
+	void OnResize(
+		UINT width,
+		UINT height
+		);
+
+	static LRESULT CALLBACK WndProc(
+		HWND hWnd,
+		UINT message,
+		WPARAM wParam,
+		LPARAM lParam
+		);
+	HWND m_hwnd;
+	ID2D1Factory* m_pDirect2dFactory;
+	ID2D1HwndRenderTarget* m_pRenderTarget;
+	ID2D1SolidColorBrush* m_pLightSlateGrayBrush;
+	ID2D1SolidColorBrush* m_pCornflowerBlueBrush;
+};
+
+Comsci::Comsci() :
+	m_hwnd(NULL),
+	m_pDirect2dFactory(NULL),
+	m_pRenderTarget(NULL),
+	m_pLightSlateGrayBrush(NULL),
+	m_pCornflowerBlueBrush(NULL)
+{
+}
+
+Comsci::~Comsci()
+{
+	SafeRelease(&m_pDirect2dFactory);
+	SafeRelease(&m_pRenderTarget);
+	SafeRelease(&m_pLightSlateGrayBrush);
+	SafeRelease(&m_pCornflowerBlueBrush);
+}
+
+void Comsci::RunMessageLoop()
+{
+	MSG msg;
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+}
+
+HRESULT Comsci::Initialize()
+{
+	HRESULT hr;
+
+	hr = CreateDeviceIndependentResources();
+
+	if (SUCCEEDED(hr))
+	{
+		//Register the window class
+		WNDCLASSEX wcex = { sizeof(WNDCLASSEX) };
+		wcex.style = CS_HREDRAW | CS_VREDRAW;
+		wcex.lpfnWndProc = Comsci::WndProc;
+		wcex.cbClsExtra = 0;
+		wcex.cbWndExtra = sizeof(LONG_PTR);
+		wcex.hInstance = HINST_THISCOMPONENT;
+		wcex.hbrBackground = NULL;
+		wcex.lpszMenuName = NULL;
+		wcex.hCursor = LoadCursor(NULL, IDI_APPLICATION);
+		wcex.lpszClassName = L"D2DComsci";
+
+		RegisterClassEx(&wcex);
+
+		FLOAT dpiX, dpiY;
+		m_pDirect2dFactory->GetDesktopDpi(&dpiX, &dpiY);
+
+		m_hwnd = CreateWindow(
+			L"D2DComsci",
+			L"Direct2D Demo App",
+			WS_OVERLAPPED,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			static_cast<UINT>(ceil(640.f * dpiX / 96.f)),
+			static_cast<UINT>(ceil(480.f * dpiY / 96.f)),
+			NULL,
+			NULL,
+			HINST_THISCOMPONENT,
+			this
+			);
+		hr = m_hwnd ? S_OK : E_FAIL;
+		if (SUCCEEDED(hr))
+		{
+			ShowWindow(m_hwnd, SW_SHOWNORMAL);
+			UpdateWindow(m_hwnd);
+		}
+	}
+
+	return hr;
+}
+
+int WINAPI WinMain(
+	HINSTANCE /* hInstance */,
+	HINSTANCE /* hPrevInstance */,
+	LPSTR /* lpCmdLine */,
+	int /* nCmdShow */
+	)
+{
+	HeapSetInformation(NULL, HeapEnableTerminationOnCorruption, NULL, 0);
+
+	if (SUCCEEDED(CoInitialize(NULL)))
+	{
+		{
+			Comsci app;
+
+			if (SUCCEEDED(app.Initialize()))
+			{
+				app.RunMessageLoop();
+			}
+		}
+		CoUninitialize();
+	}
+
+	return 0;
+}
+
+HRESULT Comsci::CreateDeviceIndependentResources()
+{
+	HRESULT hr = S_OK;
+
+	hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &m_pDirect2dFactory);
+
+	return hr;
+}
+
+HRESULT Comsci::CreateDeviceResources()
+{
+	HRESULT hr = S_OK;
+	if (!m_pRenderTarget)
+	{
+		RECT rc;
+		GetClientRect(m_hwnd, &rc);
+
+		D2D1_SIZE_U size = D2D1::SizeU(
+			rc.right - rc.left,
+			rc.bottom - rc.top
+			);
+
+		hr = m_pDirect2dFactory->CreateHwndRenderTarget(
+			D2D1::RenderTargetProperties(),
+			D2D1::HwndRenderTargetProperties(m_hwnd, size),
+			&m_pRenderTarget
+			);
+
+		if (SUCCEEDED(hr))
+		{
+			hr = m_pRenderTarget->CreateSolidColorBrush(
+				D2D1::ColorF(D2D1::ColorF::LightSlateGray),
+				&m_pLightSlateGrayBrush
+				);
+		}
+		if (SUCCEEDED(hr))
+		{
+			hr = m_pRenderTarget->CreateSolidColorBrush(
+				D2D1::ColorF(D2D1::ColorF::CornflowerBlue),
+				&m_pCornflowerBlueBrush
+				);
+		}
+	}
+
+	return hr;
+}
+
+void Comsci::DiscardDeviceResources()
+{
+	SafeRelease(&m_pRenderTarget);
+	SafeRelease(&m_pLightSlateGrayBrush);
+	SafeRelease(&m_pCornflowerBlueBrush);
+}
+
+LRESULT CALLBACK Comsci::WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	LRESULT result = 0;
+
+	if (message == WM_CREATE)
+	{
+		LPCREATESTRUCT pcs = (LPCREATESTRUCT)lParam;
+		Comsci *pComsci = (Comsci *)pcs->lpCreateParams;
+
+		::SetWindowLongPtrW(
+			hwnd,
+			GWLP_USERDATA,
+			PtrToUlong(pComsci)
+			);
+
+		result = 1;
+	}
+	else
+	{
+		Comsci *pComsci = reinterpret_cast<Comsci *>(static_cast<LONG_PTR>(
+			::GetWindowLongPtrW(
+				hwnd,
+				GWLP_USERDATA
+				)));
+
+		bool wasHandled = false;
+
+		if (pComsci)
+		{
+			switch (message)
+			{
+			case WM_SIZE:
+			{
+				UINT width = LOWORD(lParam);
+				UINT height = HIWORD(lParam);
+				pComsci->OnResize(width, height);
+			}
+			result = 0;
+			wasHandled = true;
+			break;
+
+			case WM_DISPLAYCHANGE:
+			{
+				InvalidateRect(hwnd, NULL, FALSE);
+			}
+			result = 0;
+			wasHandled = true;
+			break;
+
+			case WM_PAINT:
+			{
+				pComsci->OnRender();
+				ValidateRect(hwnd, NULL);
+			}
+			result = 0;
+			wasHandled = true;
+			break;
+
+			case WM_DESTROY:
+			{
+				PostQuitMessage(0);
+			}
+			result = 1;
+			wasHandled = true;
+			break;
+			}
+		}
+
+		if (!wasHandled)
+		{
+			result = DefWindowProc(hwnd, message, wParam, lParam);
+		}
+	}
+
+	return result;
+}
+
+HRESULT Comsci::OnRender()
+{
+	HRESULT hr = S_OK;
+	hr = CreateDeviceResources();
+
+	if (SUCCEEDED(hr))
+	{
+		m_pRenderTarget->BeginDraw();
+		m_pRenderTarget->SetTransform(D2D1::Matrix3x2F::Identity());
+		m_pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::Black));
+
+		D2D1_SIZE_F rtSize = m_pRenderTarget->GetSize();
+
+		int width = static_cast<int>(rtSize.width);
+		int height = static_cast<int>(rtSize.height);
+
+		for (int x = 0; x < width; x += 10)
+		{
+			m_pRenderTarget->DrawLine(
+				D2D1::Point2F(static_cast<FLOAT>(x), 0.0f),
+				D2D1::Point2F(static_cast<FLOAT>(x), rtSize.height),
+				m_pLightSlateGrayBrush,
+				0.5f
+				);
+		}
+
+		for (int y = 0; y < height; y += 10)
+		{
+			m_pRenderTarget->DrawLine(
+				D2D1::Point2F(0.0f, static_cast<FLOAT>(y)),
+				D2D1::Point2F(rtSize.width, static_cast<FLOAT>(y)),
+				m_pLightSlateGrayBrush,
+				0.5f
+				);
+		}
+
+		D2D1_RECT_F rectangle1 = D2D1::RectF(
+			rtSize.width / 2 - 50.0f,
+			rtSize.height / 2 - 50.0f,
+			rtSize.width / 2 + 50.0f,
+			rtSize.height / 2 + 50.0f
+			);
+
+		D2D1_RECT_F rectangle2 = D2D1::RectF(
+			rtSize.width / 2 - 100.0f,
+			rtSize.height / 2 - 100.0f,
+			rtSize.width / 2 + 100.0f,
+			rtSize.height / 2 + 100.0f
+			);
+
+		m_pRenderTarget->FillRectangle(&rectangle1, m_pLightSlateGrayBrush);
+
+		m_pRenderTarget->DrawRectangle(&rectangle2, m_pCornflowerBlueBrush);
+
+		hr = m_pRenderTarget->EndDraw();
+
+	}
+
+	if (hr == D2DERR_RECREATE_TARGET)
+	{
+		hr = S_OK;
+		DiscardDeviceResources();
+	}
+
+	return hr;
+}
+
+void Comsci::OnResize(UINT width, UINT height)
+{
+	if (m_pRenderTarget)
+	{
+		m_pRenderTarget->Resize(D2D1::SizeU(width, height));
+	}
+}
