3c84008dd96d43e6c7d5c3b9c006cef303663cad
Zirientis <Zirientis@users.noreply.github.com>
2016-05-12 21:25:58 -0700

    Get rid of useless check

--- Comsci/texpath.h
+++ Comsci/texpath.h
@@ -2,6 +2,3 @@
 
 #define TEXTURES_DIR
 // Path to the 'textures' folder goes above, with trailing backslash.
-#ifndef TEXTURES_DIR
-#error "You must specify a directory where textures are located!"
-#endif
