2b5b30c2f6bb832c2ee5ab6105447b8fe29447ae
Zirientis <Zirientis@users.noreply.github.com>
2016-08-06 21:52:50 -0700

    Switch to Boost to prepare for rearchitecting.
    Switching to an asynchronous model for game operations, using boost::asio.

--- Comsci/version.h
+++ Comsci/version.h
@@ -1,6 +1,10 @@
 #pragma once
+
+#define RELEASE_VER L"0.3"
+#define RELEASE_TYPE L"alpha"
+
 #ifdef _DEBUG
-#define COMSCI_VERSION_STR __DATE__ L" " __TIME__
+#define COMSCI_VERSION_STR __DATE__ L" " __TIME__ RELEASE_VER L"-debug"
 #else
-#define COMSCI_VERSION_STR L"0.3alpha (" __DATE__ L" " __TIME__ L")"
+#define COMSCI_VERSION_STR RELEASE_VER RELEASE_TYPE L" (" __DATE__ L" " __TIME__ L")"
 #endif
